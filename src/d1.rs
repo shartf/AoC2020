194aaf909da649154032337d924c72bb2743596a
Sergej Hartfil <sergej.hartfil@sap.com>
2022-11-23 13:56:15 +0100

    Fixes, part 2

--- src/d1.rs
+++ src/d1.rs
@@ -1,17 +1,22 @@
 use itertools::Itertools;
 
 pub fn day_1() -> anyhow::Result<()> {
-    let (a, b) = include_str!("day1.txt")
-        .split('\n')
+    let (a, b, c) = include_str!("day1.txt")
+        .trim()
+        .lines()
+        .map(|a| {
+            dbg!(a);
+            a
+        })
         .map(str::parse::<i64>)
         .collect::<Result<Vec<_>, _>>()?
         .into_iter()
         .tuple_combinations()
-        .find(|(a, b)| a + b == 2020)
+        .find(|(a, b, c)| a + b + c == 2020)
         .expect("no  pair had a sum of 2020");
 
-    dbg!(a + b);
-    dbg!(a * b);
+    dbg!(a + b + c);
+    dbg!(a * b * c);
 
     Ok(())
 }
